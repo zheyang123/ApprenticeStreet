9cf92038dc1fd4327f8bcee122839bf8932de2f4
yangy <yuzy-wm19@student.tarc.edu.my>
2023-05-12 01:57:35 +0800

    make a backup

--- app/src/main/java/my/rjtechnology/apprenticestreet/ui/progress/ProgressViewModel.kt
+++ app/src/main/java/my/rjtechnology/apprenticestreet/ui/progress/ProgressViewModel.kt
@@ -12,7 +12,9 @@ class ProgressViewModel : ViewModel() {
     private val _companyName = MutableLiveData<List<String>>().apply {
         value = listOf("item1", "item2", "item3")
     }
+    val arrayList = ArrayList<String>()
     val company = listOf("item1", "item2", "item3")
     val text: LiveData<String> = _text
     val companyName: LiveData<List<String>> = _companyName
+    val haveJob:Boolean = false
 }

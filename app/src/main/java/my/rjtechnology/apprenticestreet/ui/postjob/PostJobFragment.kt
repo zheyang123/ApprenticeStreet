7a53439ea0f5826a7cd9b1b3e5dca7affc07dcd2
ii887522 <ii887522@gmail.com>
2023-05-11 12:18:31 +0800

    WIP Post Job Page

---
+++ app/src/main/java/my/rjtechnology/apprenticestreet/ui/postjob/PostJobFragment.kt
@@ -0,0 +1,17 @@
+package my.rjtechnology.apprenticestreet.ui.postjob
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.rjtechnology.apprenticestreet.R
+
+class PostJobFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_post_job, container, false)
+    }
+}

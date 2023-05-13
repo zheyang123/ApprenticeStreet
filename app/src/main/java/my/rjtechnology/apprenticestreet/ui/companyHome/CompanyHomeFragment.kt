169f700a7b1a15f361789ac74cbab1e502b6012e
yangy <yuzy-wm19@student.tarc.edu.my>
2023-05-13 23:45:48 +0800

    added company page

---
+++ app/src/main/java/my/rjtechnology/apprenticestreet/ui/companyHome/CompanyHomeFragment.kt
@@ -0,0 +1,24 @@
+package my.rjtechnology.apprenticestreet.ui.companyHome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.rjtechnology.apprenticestreet.R
+
+
+
+class CompanyHomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_company_home, container, false)
+    }
+
+
+}
